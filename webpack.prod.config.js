4a08347d80a2ecb423403ffb2fb93bf1b7152106
mecheverria <matthew.echeverria@turner.com>
2018-09-03 14:28:58 -0400

    Updates to linting and typescript config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+const TSLintPlugin = require('tslint-webpack-plugin')
 const path = require('path')
 
 const ROOT_DIR = path.resolve(__dirname)
@@ -16,8 +17,11 @@ module.exports = {
 	module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
+            { 
+				test: /\.tsx?$/, 
+				loader: "awesome-typescript-loader", 
+				options:{ configFileName: 'tsconfig.json'} 
+			},
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
@@ -28,6 +32,9 @@ module.exports = {
 		//publicPath: '/'  //Hot reloading won’t work as expected for nested routes without it
 	},
 	plugins: [
+		new TSLintPlugin({
+            files: ['./src/**/*.ts']
+        }),
 		//HtmlWebpackPlugin will generate the index.html file
 		new HtmlWebpackPlugin({
 			favicon: 'public/favicon.ico',
