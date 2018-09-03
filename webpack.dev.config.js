ba103e1976b282eb47874d14a7e9a1e95a1c3de1
mecheverria <matthew.echeverria@turner.com>
2018-09-03 14:28:58 -0400

    Updates to linting and typescript config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
+const TSLintPlugin = require('tslint-webpack-plugin')
+
 
 const ROOT_DIR = path.resolve(__dirname)
 const SRC_DIR = path.resolve(__dirname, 'dist/temp')
@@ -23,8 +25,11 @@ module.exports = {
 	module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
+            { 
+				test: /\.tsx?$/, 
+				loader: "awesome-typescript-loader", 
+				options:{ configFileName: 'tsconfig.json'} 
+			},
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
@@ -35,13 +40,16 @@ module.exports = {
 		//publicPath: '/' // Hot reloading won’t work as expected for nested routes without it
 	},
 	plugins: [
+		new TSLintPlugin({
+            files: ['./src/**/*.ts']
+        }),
 		//HtmlWebpackPlugin will generate the index.html file
 		new HtmlWebpackPlugin({
 			favicon: 'public/favicon.ico',
 			template: 'src/client/index.ejs',
 			title: 'Node React App',
 			libraryTarget: 'window'
-		})
+		}),
 	],
 	resolve: {
 			// Add '.ts' and '.tsx' as resolvable extensions.
